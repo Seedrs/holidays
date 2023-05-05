aa9472282750ff7da9233096b7604df0d461562f
tostasmistas <margarida.reis@seedrs.com>
2023-05-05 15:30:31 +0100

    feat: consider bank holiday for the Coronation of King Charles III
    this PR is a continuation from PR https://github.com/Seedrs/holidays/pull/1

--- test/defs/test_defs_europe.rb
+++ test/defs/test_defs_europe.rb
@@ -593,6 +593,12 @@ class EuropeDefinitionTests < Test::Unit::TestCase  # :nodoc:
 
     assert_nil (Holidays.on(Date.civil(2021, 9, 19), [:gb])[0] || {})[:name]
 
+    assert_equal "Bank Holiday for the Coronation of King Charles III", (Holidays.on(Date.civil(2023, 5, 8), [:gb])[0] || {})[:name]
+
+    assert_nil (Holidays.on(Date.civil(2024, 5, 8), [:gb])[0] || {})[:name]
+
+    assert_nil (Holidays.on(Date.civil(2022, 5, 8), [:gb])[0] || {})[:name]
+
     assert_equal "Bank Holiday", (Holidays.on(Date.civil(2023, 5, 29), [:gb])[0] || {})[:name]
 
     assert_equal "Nova godina", (Holidays.on(Date.civil(2012, 1, 1), [:hr], [:informal])[0] || {})[:name]
